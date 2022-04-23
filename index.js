28602600ad8970162e169ec0147c8ceca8779341
kumabio <kumabio.prince@student.moringaschool.com>
2022-04-23 07:15:44 +0000

    completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution in this file!
+const employee = {
+    name : "Prince",
+    streetAddress : "Rosehip 122"
+  };
+  
+  
+  
+  function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newObject = {...employee};
+    newObject[key]=value;
+    return newObject;
+  }
+  
+  console.log(updateEmployeeWithKeyAndValue(employee, "name", "Pedaiah"));
+  
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    const newObject = employee;
+    newObject[key]=value;
+    return newObject;
+  }
+  
+  function deleteFromEmployeeByKey(employee,key){
+    const newObject = {...employee};
+    delete newObject[key];
+    return newObject;
+  }
+  
+  console.log(destructivelyDeleteFromEmployeeByKey(employee,"name"));
+  
+  function destructivelyDeleteFromEmployeeByKey(employee,key){
+    const newObject = employee;
+    delete newObject[key];
+    return newObject;
+  }
